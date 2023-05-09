2880f7aef574ba1e7d6f041251537e849db84004
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-09 21:30:05 +0200

    Automatic transliteration of added words

--- _app/words/models.py
+++ _app/words/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from learn_serbian.utils import transliterate
 
 
 class Word(models.Model):
     title = models.CharField(max_length=255)
     part = models.CharField(max_length=32)
+
+
+@receiver(pre_save, sender=Word)
+def word_pre_save(sender, instance, **kwargs):
+    instance.title = transliterate(instance.title)
+
+    return instance
