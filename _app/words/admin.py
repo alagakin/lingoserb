4049f4ca801863d504ac135e5b2f174f020f781d
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-09 21:50:11 +0200

    translation - admin settings

--- _app/words/admin.py
+++ _app/words/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from words.models import Word
+from words.models import Word, Translation
 
 
 class WordAdmin(admin.ModelAdmin):
     list_display = ['title', 'part']
 
 
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ['title', 'lang', 'word']
+
+
 admin.site.register(Word, WordAdmin)
+admin.site.register(Translation, TranslationAdmin)
