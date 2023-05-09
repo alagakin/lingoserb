b2cb152fbf40ebe53025f29a977375247f328949
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-09 21:47:50 +0200

    translation - model

--- _app/words/models.py
+++ _app/words/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-
 from learn_serbian.utils import transliterate
 
 
@@ -15,3 +14,14 @@ def word_pre_save(sender, instance, **kwargs):
     instance.title = transliterate(instance.title)
 
     return instance
+
+
+class Translation(models.Model):
+    title = models.CharField(max_length=255)
+    LANG_CHOICES = [
+        ('ru', 'Russian'),
+        ('eng', 'English'),
+    ]
+    lang = models.CharField(max_length=3, choices=LANG_CHOICES)
+    word = models.ForeignKey(Word, related_name='translations',
+                             on_delete=models.CASCADE)
