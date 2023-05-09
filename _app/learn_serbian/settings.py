3542e13b298a3bd05c74d4cf9672d6f71365eddc
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-09 20:56:51 +0200

    Words app

--- _app/learn_serbian/settings.py
+++ _app/learn_serbian/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "whitenoise.runserver_nostatic",
-    'accounts'
+    'accounts',
+    'words'
 ]
 
 MIDDLEWARE = [
