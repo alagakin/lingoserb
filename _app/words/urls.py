b0eab123de315b5bf3935282c5f451ae3cbc97b8
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-10 23:05:37 +0200

    SuccessRepetitionAPIView

--- _app/words/urls.py
+++ _app/words/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from words.views import SavedWordListAPIView, SavedWordCreateAPIView, \
-    DestroySavedWordAPIView, GetGameAPIView
+    DestroySavedWordAPIView, GetGameAPIView, SuccessRepetitionAPIView
 
 urlpatterns = [
     path('saved/<int:user_id>/', SavedWordListAPIView.as_view()),
     path('saved/add/', SavedWordCreateAPIView.as_view()),
     path('saved/word/<int:pk>/',
          DestroySavedWordAPIView.as_view()),
-    path('game/', GetGameAPIView.as_view())
+    path('game/', GetGameAPIView.as_view()),
+    path('game/<int:pk>/success/', SuccessRepetitionAPIView.as_view())
 ]
--- _app/words/views.py
+++ _app/words/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -38,3 +39,14 @@ class GetGameAPIView(APIView):
 
         serialized = WordsGameSerializer(saved_words, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
+
+
+class SuccessRepetitionAPIView(APIView):
+    permission_classes = (UserOwsSavedWord,)
+
+    def patch(self, request, *args, **kwargs):
+        saved_word = SavedWord.objects.get(id=kwargs['pk'])
+        saved_word.repetition_count += 1
+        saved_word.last_repetition = timezone.now()
+        saved_word.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
