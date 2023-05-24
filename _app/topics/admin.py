ed02929e34ddf6ae5730d7f362114a083c8ec7a9
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-24 21:02:44 +0200

    topics - admin

--- _app/topics/admin.py
+++ _app/topics/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from topics.models import Topic
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['id', 'words_count', 'title', 'picture', 'parent']
+    readonly_fields = ['get_words']
+
+    def get_words(self, obj):
+        return ", ".join(obj.words.values_list('title', flat=True))
+
+    get_words.short_description = 'Words'
+
+
+admin.site.register(Topic, TopicAdmin)
