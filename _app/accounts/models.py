f0f3dc58b8a4a7d7af6119206aee3b5c1301b912
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-08 08:47:25 +0200

    validation file upload

--- _app/accounts/models.py
+++ _app/accounts/models.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.exceptions import ValidationError
+
+
+def validate_image_size(image):
+    max_size = 1 * 1024 * 1024  # 2 MB
+
+    if image.size > max_size:
+        raise ValidationError(
+            f"The maximum file size allowed is 1 MB.")
 
 
 class CustomUser(AbstractUser):
-    picture = models.ImageField(upload_to='media', blank=True)
+    picture = models.ImageField(upload_to='media', blank=True,
+                                validators=[validate_image_size])
 
     LANG_CHOICES = [
         ('ru', 'Russian'),
