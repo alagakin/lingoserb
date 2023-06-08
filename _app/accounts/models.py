31be8f1f8260b2f44d2471c7ecfa29525c2401bf
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-08 07:39:57 +0200

    picture and lang for user

--- _app/accounts/models.py
+++ _app/accounts/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
+    picture = models.ImageField(upload_to='media', blank=True)
+
+    LANG_CHOICES = [
+        ('ru', 'Russian'),
+        ('en', 'English'),
+    ]
+    lang = models.CharField(max_length=3, choices=LANG_CHOICES, null=False,
+                            default='ru')
+
     def __str__(self):
         return self.username
