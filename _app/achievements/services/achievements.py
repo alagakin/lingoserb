313a110de5d529597480ad0041c9071d40827e45
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-12 16:45:44 +0200

    localization for achievements

--- _app/achievements/services/achievements.py
+++ _app/achievements/services/achievements.py
@@ -52,91 +52,91 @@ class Achievements:
         achievements = [
             {
                 'id': 1,
-                'title': _('5_words.title'),
+                'title': _('5_words'),
                 'achieved': calc_n_words(saved, 5)
             },
             {
                 'id': 2,
-                'title': _('10_words.title'),
+                'title': _('10_words'),
                 'achieved': calc_n_words(saved, 10),
                 'percent': calc_words_percent(saved, 10)
             },
             {
                 'id': 3,
-                'title': _('50_words.title'),
+                'title': _('50_words'),
                 'achieved': calc_n_words(saved, 50),
                 'percent': calc_words_percent(saved, 50),
             },
             {
                 'id': 4,
-                'title': _('250_words.title'),
+                'title': _('250_words'),
                 'achieved': calc_n_words(saved, 250),
                 'percent': calc_words_percent(saved, 250),
 
             },
             {
                 'id': 5,
-                'title': _('1000_words.title'),
+                'title': _('1000_words'),
                 'achieved': calc_n_words(saved, 1000),
                 'percent': calc_words_percent(saved, 1000),
             },
             {
                 'id': 6,
-                'title': _('5000_words.title'),
+                'title': _('5000_words'),
                 'achieved': calc_n_words(saved, 5000),
                 'percent': calc_words_percent(saved, 5000),
             },
             {
                 'id': 7,
-                'title': _('achievements.1_lessons.title'),
+                'title': _('1_lessons'),
                 'achieved': calc_n_lessons(lessons, 1),
                 'percent': calc_lessons_percent(lessons, 1),
             },
             {
                 'id': 8,
-                'title': _('achievements.5_lessons.title'),
+                'title': _('5_lessons'),
                 'achieved': calc_n_lessons(lessons, 5),
                 'percent': calc_lessons_percent(lessons, 5),
             },
             {
                 'id': 9,
-                'title': _('achievements.10_lessons.title'),
+                'title': _('10_lessons'),
                 'achieved': calc_n_lessons(lessons, 10),
                 'percent': calc_lessons_percent(lessons, 10),
             },
             {
                 'id': 10,
-                'title': _('achievements.50_lessons.title'),
+                'title': _('50_lessons'),
                 'achieved': calc_n_lessons(lessons, 50),
                 'percent': calc_lessons_percent(lessons, 50),
             },
             {
                 'id': 11,
-                'title': _('achievements.100_lessons.title'),
+                'title': _('100_lessons'),
                 'achieved': calc_n_lessons(lessons, 100),
                 'percent': calc_lessons_percent(lessons, 100),
             },
             {
                 'id': 12,
-                'title': _('achievements.500_lessons.title'),
+                'title': _('500_lessons'),
                 'achieved': calc_n_lessons(lessons, 500),
                 'percent': calc_lessons_percent(lessons, 500),
             },
             {
                 'id': 13,
-                'title': _('achievements.1000_lessons.title'),
+                'title': _('1000_lessons'),
                 'achieved': calc_n_lessons(lessons, 1000),
                 'percent': calc_lessons_percent(lessons, 1000),
             },
             {
                 'id': 14,
-                'title': _('achievements.5000_lessons.title'),
+                'title': _('5000_lessons'),
                 'achieved': calc_n_lessons(lessons, 5000),
                 'percent': calc_lessons_percent(lessons, 5000),
             },
             {
                 'id': 15,
-                'title': _('achievements.10000_lessons.title'),
+                'title': _('10000_lessons'),
                 'achieved': calc_n_lessons(lessons, 10000),
                 'percent': calc_lessons_percent(lessons, 10000),
             }
